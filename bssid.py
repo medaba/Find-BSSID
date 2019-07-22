b33755c94b8d18a9a62626cdb035d5f82375d1ac
medaba <medaba@protonmail.com>
2019-07-23 01:27:24 +0700

    descriptions

--- bssid.py
+++ bssid.py
@@ -9,11 +9,10 @@ from fire import Fire
 
 
 def get_bssid(bssid=None):
-    """
-    Принимает BSSID в качестве аргумента, а если его там нет ->
-    Извлекает BSSID из командной строки, или из буфера обмена. ->
-    Возвращает строку с BSSID без знаков препинания
-    """
+    """Принимает BSSID в качестве аргумента, а если его там нет ->
+       Извлекает BSSID из командной строки, или из буфера обмена. ->
+       Возвращает строку с BSSID без знаков препинания
+       """
     if bssid == None:
         if len(sys.argv) > 1:
             bssid = sys.argv[1]
@@ -28,15 +27,13 @@ def get_bssid(bssid=None):
 
 
 def get_coordinates(bssid=None):
-    """
-    Делает запрос к maps.yandex и передает ему полученный BSSID.
-    Возвращает координаты этой точки, либо 'Not found' в случае неудачи
-    """
+    """Делает запрос к maps.yandex и передает ему полученный BSSID.
+       Возвращает координаты этой точки, либо 'Not found' в случае неудачи
+       """
     bssid = get_bssid(bssid)
     r = requests.get(f'http://mobile.maps.yandex.net/cellid_location/?clid=-1&lac=-1&cellid=-1&operatorid=null&countrycode=null&signalstrength=-1&wifinetworks={bssid}:-65&app')
-    error = r.text.find('error')
 
-    if error > 0:
+    if "error" in r.text:
         return 'Not found'
     else:
         xml = et.fromstring(r.text)
@@ -46,27 +43,30 @@ def get_coordinates(bssid=None):
     return f'{latitude},{longitude}'
 
 
-def get_coords_list(bssid_list):
-    """
-    Принимает список bssid
-    Пытается найти эти адреса
-    В случае успеха добавляет координаты в словарь
-    Возвращает словарь вида 'bssid': 'latitude,longitude'
-    """
+def get_coords_dict(bssid_list):
+    """Принимает список bssid
+       Пытается найти эти адреса
+       В случае успеха добавляет координаты в словарь
+       Возвращает словарь вида 'bssid': 'latitude,longitude'
+       """
     coords = {}
 
-    for x in bssid_list:
-        check_bssid = get_coordinates(x)
+    for bssid in bssid_list:
+        checked = get_coordinates(bssid)
 
-        if check_bssid == 'Not found':
+        if checked == 'Not found':
             continue
         else:
-            coords[x] = check_bssid
+            coords[bssid] = checked
 
     return coords
 
 
 def open_map(bssid=None):
+    """Принимает BSSID
+       Передает его далее для поиска координат
+       В случае успеха открывает в браузере карту с данными координатами
+       """
     coordinates = get_coordinates(bssid)
     if coordinates != 'Not found':
         webbrowser.open(f'https://maps.yandex.ru/?text={coordinates}')
@@ -76,23 +76,13 @@ def open_map(bssid=None):
 
 
 
-def main(mapit=True):
 
-    if mapit:
-        open_map()
-    else:
-        print(get_coordinates())
+def main():
 
-    crds = get_coords_list(['20:4E:7F:E0:89:F8',
-                            '00:22:B0:F3:B9:D9',
-                            '00:4F:62:2D:D5:2C',
-                            'B0:48:7A:AB:FB:88',
-                            'A0:63:91:6F:1C:E7'])
-
-    print(crds)
+    # open_map('C8:3A:35:3E:95:18')
+    print(get_coords_dict(['C8:3A:35:3E:95:18']))
 
 
 
 if __name__ == '__main__':
    Fire(main)
-
